25367719c36ea799514556d645b3a5ac55496322
eddyamon <eddyamon@gmail.com>
2023-09-29 17:58:01 -0700

    create_static_lib.sh

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -12,17 +12,17 @@ int _atoi(char *s)
 
 	unsigned int res = 0;
 
- 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
- {
- 	if (s[i] == '-')
- 	sign *= -1;
- 	i++;
- }
- 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
- {
- 	res = (res * 10) + (s[i] - '0');
- 	i++;
- }
- 	res *= sign;
- 	return (res);
- }
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+{
+	if (s[i] == '-')
+	sign *= -1;
+	i++;
+}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+{
+	res = (res * 10) + (s[i] - '0');
+	i++;
+}
+	res *= sign;
+	return (res);
+}
